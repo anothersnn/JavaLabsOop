d6ed06aa84dd0eed1472a07ece856c15af895a27
anothersnn <aar2d1@mail.ru>
2021-10-07 17:12:01 +0300

    Primes.java

--- src/laba1/Palindrome.java
+++ src/laba1/Palindrome.java
@@ -1,3 +1,5 @@
+package laba1;
+
 public class Palindrome {
     public static void main(String[] args) {
         for (int i=0; i<args.length; i++) {
--- src/laba1/Primes.java
+++ src/laba1/Primes.java
@@ -1,3 +1,5 @@
+package laba1;
+
 public class Primes {
     public static void main (String[] args) {
         for (int i=2;i<101;i++) {
